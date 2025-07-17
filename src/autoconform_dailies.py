20183391fbe1ccbc5510edd2720dca02b4ec1000
s.privato <privatik08@gmail.com>
2025-07-17 12:43:28 +0300

    Пофиксил open log на win и установил дефолт тру на слейт чекбокс  autoconform

--- src/autoconform_dailies.py
+++ src/autoconform_dailies.py
@@ -165,6 +165,81 @@ class EDLParser_v3:
             else:
                 i += 1
 
+import re
+from dataclasses import dataclass
+
+class EDLParser_v3_1:
+    """
+    Класс-итератор. Итерирует EDL-файл, возвращая только те пары,
+    где есть строка данных (000xxx) и соответствующая *LOC строка.
+    """
+
+    @dataclass
+    class EDLEntry:
+        """
+        Класс-контейнер для данных из двух строк EDL
+        """
+        edl_record_id: str
+        edl_shot_name: str
+        edl_track_type: str
+        edl_transition: str
+        edl_source_in: str
+        edl_source_out: str
+        edl_record_in: str
+        edl_record_out: str
+        retime: bool  
+
+    def __init__(self, edl_path):
+        self.edl_path = edl_path
+
+    def __iter__(self):
+        with open(self.edl_path, 'r') as edl_file:
+            lines = edl_file.readlines()
+
+        i = 0
+        while i < len(lines):
+            line = lines[i].strip()
+
+            if re.match(r'^\d{6}\s', line):  # Найдена основная запись
+                parts = line.split()
+                if len(parts) < 8:
+                    i += 1
+                    continue
+
+                # Ищем LOC и M2 между текущей и следующей основной строкой
+                shot_name = None
+                retime_val = False
+                j = i + 1
+                while j < len(lines):
+                    next_line = lines[j].strip()
+
+                    if re.match(r'^\d{6}\s', next_line):  # Следующий блок
+                        break
+
+                    if next_line.startswith("M2"):
+                        retime_val = True
+
+                    loc_match = re.search(r'^\*LOC.*\s+(\S+)$', next_line)
+                    if loc_match:
+                        shot_name = loc_match.group(1)
+
+                    j += 1
+
+                if shot_name:
+                    yield self.EDLEntry(
+                        edl_record_id=parts[0],
+                        edl_shot_name=shot_name,
+                        edl_track_type=parts[2],
+                        edl_transition=parts[3],
+                        edl_source_in=parts[4],
+                        edl_source_out=parts[5],
+                        edl_record_in=parts[6],
+                        edl_record_out=parts[7],
+                        retime=retime_val,
+                    )
+                i = j  # Продвигаемся к следующему блоку
+            else:
+                i += 1
 
 class OTIOCreator:
     """
@@ -1230,6 +1305,7 @@ class Autoconform(QWidget, ConformCheckerMixin):
         bottom_row_layout.addStretch()
 
         self.include_slate = QCheckBox("Include slate")
+        self.include_slate.setChecked(True)
         bottom_row_layout.addWidget(self.include_slate)
 
 
@@ -1449,7 +1525,7 @@ class Autoconform(QWidget, ConformCheckerMixin):
         log_file_path = self.is_OS(Constant.log_path)
 
         try:
-            if sys.platform == 'Windows': 
+            if sys.platform == 'win32': 
                 os.startfile(log_file_path)
             else: 
                 subprocess.Popen(['open', log_file_path])
