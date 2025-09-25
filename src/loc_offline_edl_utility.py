91e6d88bb347778740ca05514761ca29add6a2e6
s.privato <s.privato@cgfww.ru>
2025-09-25 16:30:55 +0300

    Изменение форматирования таймкода в srt loc offline

--- src/loc_offline_edl_utility.py
+++ src/loc_offline_edl_utility.py
@@ -106,7 +106,7 @@ class LogicProcessor:
         """
         base = ":".join(str(timecode).split(":")[:3])
         frames = str(timecode).split(":")[3]
-        frames_to_mmm = str(math.ceil(int(frames) * (1000 / 24)))
+        frames_to_mmm = f'{str(math.ceil(int(frames) * (1000 / 24))):03}'
         return ",".join([base, frames_to_mmm])
     
     def get_edl_data(self) -> list:
