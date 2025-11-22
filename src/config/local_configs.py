9d982f37ce8db4693def03494eb24f2e09a20751
s.privato <s.privato@cgfww.ru>
2025-11-22 23:18:13 +0300

    Добавил пути для проекта BOI local_configs.py

--- src/config/local_configs.py
+++ src/config/local_configs.py
@@ -8,5 +8,11 @@ LOCAL_CONFIGS = {
             "init_shots_root_win": "R:/CC_GORYNYCH/VFX",
             "init_shots_root_mac": "/Volumes/RAID/CC_GORYNYCH/VFX",
                 }
-            }
+            },
+    'BOI': {  # Рождение империи
+        'paths': {
+            "init_shots_root_win": "R:\CC_BOE\VFX",
+            "init_shots_root_mac": "/Volumes/RAID/CC_BOE/VFX",
+                }
+    }
 }
